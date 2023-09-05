b01241f1a2a6a9932c6ac41b8012ad0caf06c848
pyt111 <584702675@qq.com>
2023-09-05 10:37:57 +0800

    变更2

--- packages/components/Table/index.ts
+++ packages/components/Table/index.ts
@@ -1,9 +1,14 @@
 import { withInstall } from '@p-helper/utils';
 import basicTable from './src/BasicTable.vue';
+import { TableIconCell, TableTooltipHeader } from './src/components';
+
 export * from './src/types/table';
 export * from './src/types/tableAction';
 
 export * from './src/hooks/useTable';
 export * from './src/components';
 
-export const BasicTable = withInstall(basicTable);
+export const BasicTable = withInstall(basicTable, {
+  TableTooltipHeader,
+  TableIconCell,
+});
--- packages/components/Table/src/components/index.ts
+++ packages/components/Table/src/components/index.ts
@@ -1,5 +1,4 @@
-import { withInstall } from '@p-helper/utils';
 import tableIconCell from './custom/IconCell/IconCell.vue';
-import tableTooltipHeader from './custom/header/TooltipHeader.vue';
-export const TableTooltipHeader = withInstall(tableTooltipHeader);
-export const TableIconCell = withInstall(tableIconCell);
+import tooltipHeader from './custom/header/TooltipHeader.vue';
+export const TableTooltipHeader = tooltipHeader;
+export const TableIconCell = tableIconCell;
