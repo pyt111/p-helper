d14e409fcb8fa8e95e7f3f3109063f7959b954d9
pyt111 <584702675@qq.com>
2024-06-21 14:36:34 +0800

    table emit导出测试

--- packages/components/Table/src/BasicTable.vue
+++ packages/components/Table/src/BasicTable.vue
@@ -64,15 +64,15 @@
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
   import {
     computed,
-    defineComponent,
     reactive,
     ref,
     toRaw,
-    toRefs,
     unref,
+    useAttrs,
+    useSlots,
   } from 'vue';
   import { ElCard, ElTable, ElTableColumn } from 'element-plus';
   import { cloneDeep, pick } from 'lodash-es';
@@ -87,271 +87,309 @@
   import { useTableForm } from './hooks/useTableForm';
   import { useTableExpand } from './hooks/useTableExpand';
   import { basicProps, basicTableEmits } from './props';
-  import { CustomCellComponent } from './components/custom/CustomCellComponent';
+  // import { CustomCellComponent } from './components/custom/CustomCellComponent';
   import BasicColumnComponent from './BasicColumn';
   import type { BasicTableProps, TableActionType } from './types/table';
 
-  export default defineComponent({
+  defineOptions({
     name: 'BasicTable',
-    components: {
-      BasicForm,
-      ElTable,
-      ElTableColumn,
-      ElCard,
-      CustomCellComponent,
-      BasicColumnComponent,
+  });
+  const props = defineProps(basicProps);
+  const emit = defineEmits(basicTableEmits);
+  const slots = useSlots();
+  const attrs = useAttrs();
+
+  const tableElRef = ref(null);
+  const wrapRef = ref(null);
+  const formRef = ref<any>(null);
+  const bottomCardRef = ref(null);
+  const paginationElRef = ref(null);
+  const tableData = ref([]);
+  const innerPropsRef = ref();
+  const elTablePropsKeys = Object.keys(ElTable.props);
+
+  const [registerForm, formActions] = useForm({
+    isCol: false,
+  });
+
+  const state = reactive({
+    listLoading: false,
+    layout: 'total, sizes, prev, pager, next, jumper',
+    queryData: {},
+    selectionData: [] as any,
+  });
+
+  const getProps = computed(() => {
+    return { ...props, ...unref(innerPropsRef) };
+  });
+
+  const getFormConfig = computed(() => {
+    return {
+      ...getBindValues.value.formConfig,
+    };
+  });
+
+  const { getLoading, setLoading } = useLoading(getProps);
+
+  const {
+    getPaginationInfo,
+    getPagination,
+    setPagination,
+    setShowPagination,
+    getShowPagination,
+    renderPagination,
+  } = usePagination(getProps, {
+    paginationChange,
+  });
+
+  const {
+    getDataSourceRef,
+    getDataSource,
+    getRawDataSource,
+    setTableData,
+    updateTableDataRecord,
+    deleteTableDataRecord,
+    insertTableDataRecord,
+    findTableDataRecord,
+    fetch,
+    getRowKey,
+    getRowKeyName,
+    reload,
+    updateTableData,
+    getRowDataByRowIndex,
+    recordCache,
+    getCellRecord,
+  } = useDataSource(
+    getProps,
+    {
+      tableData,
+      getPaginationInfo,
+      setLoading,
+      setPagination,
+      getFieldsValue: formActions.getFieldsValue,
     },
-    props: basicProps,
+    emit
+  );
+  const { getViewColumns, getColumns } = useColumns(getProps, {
+    getRowKeyName,
+    getDataSource,
+    getRowDataByRowIndex,
+    findTableDataRecord,
+    getPaginationInfo,
+    recordCache,
+  });
 
-    emits: basicTableEmits,
+  const {
+    getExpandOption,
+    expandAll,
+    expandRows,
+    collapseAll,
+    collapseRows,
+    onExpandChange,
+  } = useTableExpand(getProps, tableData, emit);
 
-    setup(props, { emit, attrs, slots }) {
-      const tableElRef = ref(null);
-      const wrapRef = ref(null);
-      const formRef = ref<any>(null);
-      const bottomCardRef = ref(null);
-      const paginationElRef = ref(null);
-      const tableData = ref([]);
-      const innerPropsRef = ref();
-      const elTablePropsKeys = Object.keys(ElTable.props);
+  const paginationAlign = computed(
+    () =>
+      (!isBoolean(getPaginationInfo.value) && getPaginationInfo.value.align) ||
+      'center'
+  );
 
-      const [registerForm, formActions] = useForm({
-        isCol: false,
-      });
+  const getBindValues = computed(() => {
+    const dataSource = unref(getDataSourceRef);
 
-      const state = reactive({
-        listLoading: false,
-        layout: 'total, sizes, prev, pager, next, jumper',
-        queryData: {},
-        selectionData: [] as any,
-      });
+    const propsData = {
+      ...unref(getProps),
+      columns: toRaw(unref(getViewColumns)),
+      style: {},
+      ...attrs,
+      height: unref(getProps).height,
+      data: dataSource,
+      ...unref(getExpandOption),
+    };
+    const bottomHeight = propsData.noPage ? 0 : 52;
+    const maxHeight = `calc(100% - ${bottomHeight}px)`;
+    // 是否撑满
+    if (getProps.value.fullHeight) {
+      propsData.style.height = maxHeight;
+      propsData.height = '100%';
+    } else if (getProps.value.autoMaxFullHeight) {
+      propsData.maxHeight = maxHeight;
+      propsData.height = '100%';
+    }
 
-      const getProps = computed(() => {
-        return { ...props, ...unref(innerPropsRef) };
-      });
+    return propsData;
+  });
 
-      const getFormConfig = computed(() => {
-        return {
-          ...getBindValues.value.formConfig,
-        };
-      });
+  const getTableProps = computed(() => {
+    return {
+      ...unref(getExpandOption),
+      headerCellClassName: 'table-header-background', // 头部默认背景色
+      ...pick(unref(getBindValues), [...elTablePropsKeys, 'onExpandChange']),
+      onExpandChange,
+      ...attrs,
+    };
+  });
 
-      const { getLoading, setLoading } = useLoading(getProps);
+  function paginationChange() {
+    // 前端分页
+    if (getProps.value.offlinePaging) {
+      return;
+    }
+    fetch();
+  }
 
-      const {
-        getPaginationInfo,
-        getPagination,
-        setPagination,
-        setShowPagination,
-        getShowPagination,
-        renderPagination,
-      } = usePagination(getProps, {
-        paginationChange,
-      });
+  const {
+    replaceFormSlotKey,
+    getFormSlotKeys,
+    getFormProps,
+    handleSearchInfoChange,
+  } = useTableForm(getProps, slots, fetch, getLoading);
 
-      const {
-        getDataSourceRef,
-        getDataSource,
-        getRawDataSource,
-        setTableData,
-        updateTableDataRecord,
-        deleteTableDataRecord,
-        insertTableDataRecord,
-        findTableDataRecord,
-        fetch,
-        getRowKey,
-        getRowKeyName,
-        reload,
-        updateTableData,
-        getRowDataByRowIndex,
-        recordCache,
-        getCellRecord,
-      } = useDataSource(
-        getProps,
-        {
-          tableData,
-          getPaginationInfo,
-          setLoading,
-          setPagination,
-          getFieldsValue: formActions.getFieldsValue,
-        },
-        emit
-      );
-      const { getViewColumns, getColumns } = useColumns(getProps, {
-        getRowKeyName,
-        getDataSource,
-        getRowDataByRowIndex,
-        findTableDataRecord,
-        getPaginationInfo,
-        recordCache,
-      });
+  const setProps = (props: Partial<BasicTableProps>) => {
+    innerPropsRef.value = { ...unref(innerPropsRef), ...props };
+  };
 
-      const {
-        getExpandOption,
-        expandAll,
-        expandRows,
-        collapseAll,
-        collapseRows,
-        onExpandChange,
-      } = useTableExpand(getProps, tableData, emit);
-
-      const paginationAlign = computed(
-        () =>
-          (!isBoolean(getPaginationInfo.value) &&
-            getPaginationInfo.value.align) ||
-          'center'
-      );
-
-      const getBindValues = computed(() => {
-        const dataSource = unref(getDataSourceRef);
-
-        const propsData = {
-          ...unref(getProps),
-          columns: toRaw(unref(getViewColumns)),
-          style: {},
-          ...attrs,
-          height: unref(getProps).height,
-          data: dataSource,
-          ...unref(getExpandOption),
-        };
-        const bottomHeight = propsData.noPage ? 0 : 52;
-        const maxHeight = `calc(100% - ${bottomHeight}px)`;
-        // 是否撑满
-        if (getProps.value.fullHeight) {
-          propsData.style.height = maxHeight;
-          propsData.height = '100%';
-        } else if (getProps.value.autoMaxFullHeight) {
-          propsData.maxHeight = maxHeight;
-          propsData.height = '100%';
-        }
-
-        return propsData;
-      });
-
-      const getTableProps = computed(() => {
-        return {
-          ...unref(getExpandOption),
-          headerCellClassName: 'table-header-background', // 头部默认背景色
-          ...pick(unref(getBindValues), [
-            ...elTablePropsKeys,
-            'onExpandChange',
-          ]),
-          onExpandChange,
-          ...attrs,
-        };
-      });
-
-      function paginationChange() {
-        // 前端分页
-        if (getProps.value.offlinePaging) {
-          return;
-        }
-        fetch();
-      }
-
-      const {
-        replaceFormSlotKey,
-        getFormSlotKeys,
-        getFormProps,
-        handleSearchInfoChange,
-      } = useTableForm(getProps, slots, fetch, getLoading);
-
-      const setProps = (props: Partial<BasicTableProps>) => {
-        innerPropsRef.value = { ...unref(innerPropsRef), ...props };
-      };
-
-      const tableAction: TableActionType = {
-        reload,
-        getColumns,
-        getRowDataByRowIndex,
-        // refreshOfflineTableData,
-        renderPagination,
-        getDataSource,
-        getRawDataSource,
-        setTableData,
-        setLoading,
-        updateTableDataRecord,
-        deleteTableDataRecord,
-        insertTableDataRecord,
-        findTableDataRecord,
-        updateTableData,
-        setShowPagination,
-        getShowPagination,
-        getPaginationRef: getPagination,
-        setPagination,
-        redoHeight: () => {},
-        expandRows,
-        collapseRows,
-        expandAll,
-        collapseAll,
-        getTableData: () => {
-          return unref(tableData);
-        },
-        getPagination: () => {
-          return unref(getPaginationInfo);
-        },
-        setProps,
-        getSelectionData: () => cloneDeep(state.selectionData),
-        getCellRecord,
-        emit,
-      };
-
-      createTableContext({ ...tableAction, wrapRef, getBindValues });
-
-      const setSelectRows = (rows) => {
-        // console.log('setSelectRows');
-        emit('selectionChange', rows);
-      };
-
-      const onSelect = (selection: Record<string, any>[]) => {
-        state.selectionData = selection;
-      };
-
-      const onCellDblclick = (row, col) => {
-        const key = updateTableCellStatusKey(col.prop);
-        if (key) {
-          row[key] = true; // 打开自定义vNode
-        }
-      };
-
-      emit('register', tableAction, formActions);
-
-      return {
-        // data
-        ...toRefs(state),
-        renderPagination,
-        tableData,
-        // pagination,
-        getBindValues,
-        tableAction,
-        getFormConfig,
-        replaceFormSlotKey,
-        getFormSlotKeys,
-        getFormProps,
-        getTableProps,
-        getDataSourceRef,
-        getViewColumns,
-        getProps,
-        getRowKey,
-        getRowKeyName,
-        elTablePropsKeys,
-        recordCache,
-
-        setSelectRows,
-        onSelect,
-        onCellDblclick,
-        updateTableCellStatusKey,
-        registerForm,
-        handleSearchInfoChange,
-        paginationAlign,
-
-        getLoading,
-        wrapRef,
-        tableElRef,
-        formRef,
-        paginationElRef,
-        bottomCardRef,
-      };
+  const tableAction: TableActionType = {
+    reload,
+    getColumns,
+    getRowDataByRowIndex,
+    // refreshOfflineTableData,
+    renderPagination,
+    getDataSource,
+    getRawDataSource,
+    setTableData,
+    setLoading,
+    updateTableDataRecord,
+    deleteTableDataRecord,
+    insertTableDataRecord,
+    findTableDataRecord,
+    updateTableData,
+    setShowPagination,
+    getShowPagination,
+    getPaginationRef: getPagination,
+    setPagination,
+    redoHeight: () => {},
+    expandRows,
+    collapseRows,
+    expandAll,
+    collapseAll,
+    getTableData: () => {
+      return unref(tableData);
     },
+    getPagination: () => {
+      return unref(getPaginationInfo);
+    },
+    setProps,
+    getSelectionData: () => cloneDeep(state.selectionData),
+    getCellRecord,
+    emit,
+  };
+
+  createTableContext({ ...tableAction, wrapRef, getBindValues });
+
+  const setSelectRows = (rows) => {
+    // console.log('setSelectRows');
+    emit('selectionChange', rows);
+  };
+
+  const onSelect = (selection: Record<string, any>[]) => {
+    state.selectionData = selection;
+  };
+
+  const onCellDblclick = (row, col) => {
+    const key = updateTableCellStatusKey(col.prop);
+    if (key) {
+      row[key] = true; // 打开自定义vNode
+    }
+  };
+
+  emit('register', tableAction, formActions);
+  // export default defineComponent({
+  //   components: {
+  //     BasicForm,
+  //     ElTable,
+  //     ElTableColumn,
+  //     ElCard,
+  //     CustomCellComponent,
+  //     BasicColumnComponent,
+  //   },
+  //   props: basicProps,
+  //
+  //   emits: basicTableEmits,
+  //
+  //   setup(props, { emit, attrs, slots }) {
+  //
+  //     return {
+  //       // data
+  //       ...toRefs(state),
+  //       renderPagination,
+  //       tableData,
+  //       // pagination,
+  //       getBindValues,
+  //       tableAction,
+  //       getFormConfig,
+  //       replaceFormSlotKey,
+  //       getFormSlotKeys,
+  //       getFormProps,
+  //       getTableProps,
+  //       getDataSourceRef,
+  //       getViewColumns,
+  //       getProps,
+  //       getRowKey,
+  //       getRowKeyName,
+  //       elTablePropsKeys,
+  //       recordCache,
+  //
+  //       setSelectRows,
+  //       onSelect,
+  //       onCellDblclick,
+  //       updateTableCellStatusKey,
+  //       registerForm,
+  //       handleSearchInfoChange,
+  //       paginationAlign,
+  //
+  //       getLoading,
+  //       wrapRef,
+  //       tableElRef,
+  //       formRef,
+  //       paginationElRef,
+  //       bottomCardRef,
+  //     };
+  //   },
+  // });
+
+  defineExpose({
+    renderPagination,
+    tableData,
+    // pagination,
+    getBindValues,
+    tableAction,
+    getFormConfig,
+    replaceFormSlotKey,
+    getFormSlotKeys,
+    getFormProps,
+    getTableProps,
+    getDataSourceRef,
+    getViewColumns,
+    getProps,
+    getRowKey,
+    getRowKeyName,
+    elTablePropsKeys,
+    recordCache,
+
+    setSelectRows,
+    onSelect,
+    onCellDblclick,
+    updateTableCellStatusKey,
+    registerForm,
+    handleSearchInfoChange,
+    paginationAlign,
+
+    getLoading,
+    wrapRef,
+    tableElRef,
+    formRef,
+    paginationElRef,
+    bottomCardRef,
   });
 </script>
--- packages/utils/vue/install.ts
+++ packages/utils/vue/install.ts
@@ -7,7 +7,7 @@ export const withInstall = <T, E extends Record<string, any>>(
   extra?: E
 ) => {
   useGlobalConfig();
-  (main as SFCWithInstall<T>).install = (app, options): void => {
+  (main as SFCWithInstall<T>).install = (app): void => {
     for (const comp of [main, ...Object.values(extra ?? {})]) {
       app.component(comp.name, comp);
     }
@@ -15,7 +15,7 @@ export const withInstall = <T, E extends Record<string, any>>(
 
   if (extra) {
     for (const [key, comp] of Object.entries(extra)) {
-      (main as any)[key] = defineAsyncComponent(comp);
+      (main as any)[key] = comp;
     }
   }
   return main as SFCWithInstall<T> & E;
